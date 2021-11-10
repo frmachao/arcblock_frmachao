746bfe1c3d2870056db1c398acaaa27fbf1b85dd
frmachao <frmachao@126.com>
2021-11-10 11:40:24 +0800

    add: antd

--- src/pages/demo.jsx
+++ src/pages/demo.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Button } from 'antd';
 import './demo.css';
 
 const Demo = () => {
   return (
     <div className="demo_container">
       <h2>demo</h2>
+      <Button type="primary">Primary Button</Button>
+      <Button>Default Button</Button>
     </div>
   );
 };
